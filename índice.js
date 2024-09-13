b66f13c1e9ac985408846cee062eb452d9567b77
Jéssica Lima <165609457+PandoraJess@users.noreply.github.com>
2024-09-13 19:18:42 -0300

    índice.js

---
+++ índice.js
@@ -0,0 +1,48 @@
+// Olá, mundo!
+console . log ( "olá, mundo!" )
+
+
+// Grampos - deixe
+let  mensagens  =  "olá, mundo!"
+
+
+// Grampos - const
+const  mensagem  =  "olá, eu"
+{
+    const  mensagem  =  "olá, Mayk!"
+    console . log ( mensagem )
+}
+console . log ( mensagem ) ;
+
+
+// matrizes
+deixe  metas  =  [ 'mayk' ,  'alô' ]
+deixe  metas01  =  [ 2 ,  'mayk' ]
+// concatenando valores
+console . log ( metas [ 1 ]  +  ", "  +  metas [ 0 ] ) 
+
+deixe  metas02  =  [
+    metas ,
+    {
+        valor : 'caminhar 20 minutos todos os dias' ,
+        verificado : falso
+    }
+]
+console . log ( metas02 [ 1 ] . valor ) ;
+
+// objetos
+deixe  meta  =  {
+    value : 'ler um livro por mês' ,
+    endereço : 2 ,
+    verificado : verdadeiro ,
+    isChecked : ( )  =>  {
+        console . log ( informações )
+    }
+}
+console . log ( meta . valor ) ;
+
+// função // função de seta
+const  criarMeta  =  ( )  =>  { }
+
+// função nomeada
+função  criarMetas ( )  { }
